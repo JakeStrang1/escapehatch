2695dd386ae44117b143fb56991acf6b343a5692
Jake <jake@jakestrang.com>
2023-06-09 13:29:22 -0400

    Frontend: improve user summary bar

--- frontend/src/component/UserSummary.js
+++ frontend/src/component/UserSummary.js
@@ -27,11 +27,11 @@ export default class UserSummary extends React.Component {
         {/* Display on XS only */}
         <UserSummaryXS className="d-sm-none" user={this.props.user}/>
 
-        {/* Display on SM only */}
-        <UserSummarySM className="d-none d-sm-block d-md-none" user={this.props.user}/>
+        {/* Display on SM and MD only */}
+        <UserSummaryMD className="d-none d-sm-block d-lg-none" user={this.props.user}/>
 
-        {/* Display on MD and LG only */}
-        <UserSummaryMD className="d-none d-md-block d-xl-none" user={this.props.user}/>
+        {/* Display on LG only */}
+        <UserSummaryLG className="d-none d-lg-block d-xl-none" user={this.props.user}/>
 
         {/* Display on XL only */}
         <UserSummaryXL className="d-none d-xl-block" user={this.props.user}/>
@@ -41,154 +41,35 @@ export default class UserSummary extends React.Component {
 }
 
 class UserSummaryXS extends React.Component {
-    constructor(props){
-      super(props);
-  
-      this.state = {
-        user: this.props.user,
-      }
-  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      user: this.props.user,
     }
-  
-    render() {
-      return (
-        <>
-          <Container className={this.props.className} fluid>
-            <Row>
-              <Col xs={12} style={{backgroundColor:"#222"}} className="pt-3 pl-4 pb-2">
-                <Row>
-                  <Col xs={9}>
-                    <span id="username">{this.props.user.username}<span id="userNumber">#{this.props.user.number}</span></span>
-                  </Col>
-                  <Col xs={3} className="text-right">
-                    <Dropdown>
-                      <Dropdown.Toggle className="userDropDown" variant="secondary">
-                      </Dropdown.Toggle>
-                      <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Log out</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col xs={6}>
-                    <Row>
-                      <Col>
-                        <span id="fullName">{this.props.user.full_name}</span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <Badge pill type="submit" className="userStatusBtn selfStatusBtn">You</Badge>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col xs={3} className="followerColumn">
-                    <Row>
-                      <Col>
-                        <span className="followerCount">{this.props.user.follower_count}</span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                      <span className="followerLabel">Followers</span>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col xs={3} className="followerColumn">
-                    <Row>
-                      <Col>
-                      <span className="followerCount">{this.props.user.following_count}</span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                      <span className="followerLabel">Following</span>
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Container>
-        </>
-      )
-    }
+
   }
 
-class UserSummarySM extends React.Component {
-    constructor(props){
-      super(props);
-  
-      this.state = {
-        user: this.props.user,
-      }
-  
-    }
-  
-    render() {
-      return (
-        <>
-          <Container className={this.props.className} fluid>
-            <Row>
-              <Col xs={12} style={{backgroundColor:"#222"}} className="pt-3 pl-4 pb-2">
-                <Row>
-                  <Col xs={1}></Col>
-                  <Col xs={8}>
-                    <span id="username">{this.props.user.full_name}&emsp;<span style={{color: "#555"}}>&mdash;</span>&emsp;{this.props.user.username}<span id="userNumber">#{this.props.user.number}</span></span>
-                  </Col>
-                  <Col xs={2} className="text-right">
-                    <Dropdown>
-                      <Dropdown.Toggle className="userDropDown" variant="secondary">
-                      </Dropdown.Toggle>
-                      <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Log out</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </Col>
-                  <Col xs={1}></Col>
-                </Row>
-                <Row className="mt-3">
-                  <Col xs={1}></Col>
-                  <Col>
-                    <Row>
-                      <Col>
-                        <Badge pill type="submit" className="userStatusBtn selfStatusBtn">You</Badge>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col className="followerColumn">
-                    <Row>
-                      <Col>
-                        <span className="followerCount">{this.props.user.follower_count}</span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                      <span className="followerLabel">Followers</span>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col className="followerColumn">
-                    <Row>
-                      <Col>
-                      <span className="followerCount">{this.props.user.following_count}</span>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                      <span className="followerLabel">Following</span>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col xs={1}></Col>
-                </Row>
-              </Col>
-            </Row>
-          </Container>
-        </>
-      )
-    }
+  render() {
+    return (
+      <>
+        <Container className={this.props.className} fluid>
+          <Row>
+            <Col xs={12} style={{backgroundColor:"#222"}} className="pt-3 pl-4 pb-3">
+              <Row>
+                <Col>
+                  <div className="d-flex flex-row justify-content-between">
+                    <UserLabelXS user={this.props.user}/>
+                    <RightSummaryXS user={this.props.user}/>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    )
+  }
 }
 
 class UserSummaryMD extends React.Component {
@@ -208,61 +89,46 @@ class UserSummaryMD extends React.Component {
           <Row>
             <Col xs={12} style={{backgroundColor:"#222"}} className="pt-3 pl-4 pb-3">
               <Row>
-                <Col xs={2}></Col>
                 <Col>
-                  <Row>
-                    <Col xs={11}>
-                      <span id="username">{this.props.user.full_name}&emsp;<span style={{color: "#555"}}>&mdash;</span>&emsp;{this.props.user.username}<span id="userNumber">#{this.props.user.number}</span></span>
-                    </Col>
-                    <Col xs={1} className="text-right">
-                      <Dropdown>
-                        <Dropdown.Toggle className="userDropDown" variant="secondary">
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                          <Dropdown.Item href="#/action-1">Log out</Dropdown.Item>
-                        </Dropdown.Menu>
-                      </Dropdown>
-                    </Col>
-                  </Row>
-                  <Row className="mt-3">
-                    <Col>
-                      <Row>
-                        <Col>
-                          <Badge pill type="submit" className="userStatusBtn selfStatusBtn">You</Badge>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col className="followerColumn">
-                      <Row>
-                        <Col>
-                          <span className="followerCount">{this.props.user.follower_count}</span>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col>
-                        <span className="followerLabel">Followers</span>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col style={{justifyContent: "right"}} className="d-flex">
-                      <Row>
-                        <Col className="followerColumn">
-                          <Row>
-                            <Col>
-                            <span className="followerCount">{this.props.user.following_count}</span>
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col>
-                            <span className="followerLabel">Following</span>
-                            </Col>
-                          </Row>
-                        </Col>
-                      </Row>
-                    </Col>
-                  </Row>
+                  <div className="d-flex flex-row align-items-center justify-content-between">
+                    <UserLabel user={this.props.user}/>
+                    <RightSummary user={this.props.user}/>
+                  </div>
                 </Col>
-                <Col xs={2}></Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    )
+  }
+}
+
+class UserSummaryLG extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      user: this.props.user,
+    }
+
+  }
+
+  render() {
+    return (
+      <>
+        <Container className={this.props.className} fluid>
+          <Row>
+            <Col xs={12} style={{backgroundColor:"#222"}} className="pt-3 pl-4 pb-3">
+              <Row>
+                <Col xs={1}></Col>
+                <Col>
+                  <div className="d-flex flex-row align-items-center justify-content-between">
+                    <UserLabel user={this.props.user}/>
+                    <RightSummary user={this.props.user}/>
+                  </div>
+                </Col>
+                <Col xs={1}></Col>
               </Row>
             </Col>
           </Row>
@@ -289,61 +155,14 @@ class UserSummaryXL extends React.Component {
           <Row>
             <Col xs={12} style={{backgroundColor:"#222"}} className="pt-3 pl-4 pb-3">
               <Row>
-                <Col xs={3}></Col>
+                <Col xs={2}></Col>
                 <Col>
-                  <Row>
-                    <Col xs={11}>
-                      <span id="username">{this.props.user.full_name}&emsp;<span style={{color: "#555"}}>&mdash;</span>&emsp;{this.props.user.username}<span id="userNumber">#{this.props.user.number}</span></span>
-                    </Col>
-                    <Col xs={1} className="text-right">
-                      <Dropdown>
-                        <Dropdown.Toggle className="userDropDown" variant="secondary">
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                          <Dropdown.Item href="#/action-1">Log out</Dropdown.Item>
-                        </Dropdown.Menu>
-                      </Dropdown>
-                    </Col>
-                  </Row>
-                  <Row className="mt-3">
-                    <Col>
-                      <Row>
-                        <Col>
-                          <Badge pill type="submit" className="userStatusBtn selfStatusBtn">You</Badge>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col className="followerColumn">
-                      <Row>
-                        <Col>
-                          <span className="followerCount">{this.props.user.follower_count}</span>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col>
-                        <span className="followerLabel">Followers</span>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col style={{justifyContent: "right"}} className="d-flex">
-                      <Row>
-                        <Col className="followerColumn">
-                          <Row>
-                            <Col>
-                            <span className="followerCount">{this.props.user.following_count}</span>
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col>
-                            <span className="followerLabel">Following</span>
-                            </Col>
-                          </Row>
-                        </Col>
-                      </Row>
-                    </Col>
-                  </Row>
+                  <div className="d-flex flex-row align-items-center justify-content-between">
+                    <UserLabel user={this.props.user}/>
+                    <RightSummary user={this.props.user}/>
+                  </div>
                 </Col>
-                <Col xs={3}></Col>
+                <Col xs={2}></Col>
               </Row>
             </Col>
           </Row>
@@ -351,4 +170,133 @@ class UserSummaryXL extends React.Component {
       </>
     )
   }
+}
+
+class UserLabelXS extends React.Component {
+  render() {
+    return (
+      <>
+        <Col className="d-flex flex-column">
+          <Row>
+            <Col>
+              <span id="username-xl">{this.props.user.username}</span>&nbsp;&nbsp;&nbsp;
+              <span id="userNumber-xl">#{this.props.user.number}</span>
+            </Col>
+          </Row>
+          <Row className="flex-grow-1">
+            <Col className="d-flex flex-column justify-content-center">
+              <span id="fullName-xs">{this.props.user.full_name}</span>
+            </Col>
+          </Row>
+        </Col>
+      </>
+    )
+  }
+}
+
+class UserLabel extends React.Component {
+  render() {
+    return (
+      <>
+      <div className="d-inline">
+        <span id="username-xl">{this.props.user.username}</span>&emsp;
+        <span id="userNumber-xl">#{this.props.user.number}&emsp;•&emsp;</span>
+        <span id="fullName-xl">{nbsp(this.props.user.full_name)}</span>
+        
+      </div>
+    </>
+    )
+  }
+}
+
+class FollowerStatXS extends React.Component {
+  render() {
+    return (
+      <div className="text-center">
+        <Row>
+          <Col>
+            <span className="followerCount-xs">{this.props.count}</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <span className="followerLabel-xs">{this.props.label}</span>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+class FollowerStat extends React.Component {
+  render() {
+    return (
+      <div className="text-center">
+        <Row>
+          <Col>
+            <span className="followerCount">{this.props.count}</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <span className="followerLabel">{this.props.label}</span>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+class UserDropDown extends React.Component {
+  render() {
+    return (
+      <Dropdown>
+        <Dropdown.Toggle className="userDropDown" variant="secondary">
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Log out</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    )
+  }
+}
+
+class RightSummaryXS extends React.Component {
+  render() {
+    return (
+      <>
+        <Col className="rightSummary-xs">
+          <Row>
+            <Col className="text-right">
+              <UserDropDown/>
+            </Col>
+          </Row>
+          <Row className="justify-content-end mt-2">
+            <Col xs={12} className="d-flex flex-row justify-content-between">
+              <FollowerStatXS count={this.props.user.follower_count} label="Followers"/>
+              <FollowerStatXS count={this.props.user.following_count} label="Following"/>
+            </Col>
+          </Row>
+        </Col>
+      </>
+    )
+  }
+}
+
+class RightSummary extends React.Component {
+  render() {
+    return (
+      // <Row>
+        <div className="d-flex flex-row rightSummary justify-content-between">
+          <FollowerStat count={this.props.user.follower_count} label="Followers"/>
+          <FollowerStat count={this.props.user.following_count} label="Following"/>
+          <UserDropDown/>
+        </div>
+      // </Row>
+    )
+  }
+}
+
+function nbsp(str) {
+  return str.replace(/ /g, "\u00a0")
 }
